43f77b70c510c41551ada88077a5638ccebc42c9
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-07-26 20:52:05 +0100

    Update fetchMainafly.js

--- netlifly/functions/fetchMainafly.js
+++ netlifly/functions/fetchMainafly.js
@@ -1,7 +1,23 @@
 const https = require('https');
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify Function!" }),
-  };
+
+exports.handler = async function (event, context) {
+  return new Promise((resolve, reject) => {
+    https.get('https://www.google.com', (res) => {
+      let data = '';
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: { 'Content-Type': 'text/html' },
+          body: data
+        });
+      });
+    }).on('error', (err) => {
+      resolve({
+        statusCode: 500,
+        body: `Error: ${err.message}`
+      });
+    });
+  });
 };
