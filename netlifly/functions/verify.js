9d5f612d1483af85d88a4980b983043e58c55f96
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-08-05 21:57:12 +0100

    Update verify.js

--- netlifly/functions/verify.js
+++ netlifly/functions/verify.js
@@ -1,3 +1,41 @@
-const axios = require('axios');
-
 const admin = require("firebase-admin");
+
+let initialized = false;
+
+exports.handler = async function(event, context) {
+  if (!initialized) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+
+    initialized = true;
+  }
+
+  const db = admin.firestore();
+
+  // Example: Get a document from 'users' collection
+  const docRef = db.collection("students").doc("yusufmainaishaku@gmail.com");
+
+  try {
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+      return {
+        statusCode: 404,
+        body: "Document not found",
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(doc.data()),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
