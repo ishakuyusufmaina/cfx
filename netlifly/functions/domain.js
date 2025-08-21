86fbcff202d6d3f0e9b6f69f00062021de2f5782
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-08-21 19:57:01 +0100

    Update domain.js

--- netlifly/functions/domain.js
+++ netlifly/functions/domain.js
@@ -1,1 +1,43 @@
+const token = process.env.GH_TOKEN;
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: JSON.stringify({ error: "Method Not Allowed" }) };
+  }
+  const repo = JSON.parse(event.body).repoName;
+  const subdomain = JSON.parse(event.body).domain;
+  const owner = "ishakuyusufmaina";
+  const url = `https://api.github.com/repos/${owner}/${repoName}/contents/CNAME`;
+  const content = Buffer.from(`${subdomain}.mainafly.com`, "utf-8").toString("base64");
 
+  try {
+    const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/vnd.github+json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
+    const data = await response.json();
+
+   if (response.ok) {
+     return {
+       statusCode: 200,
+       body: JSON.stringify({ok: true, message: `${repoName} is published @ ${subdomain}.mainafly.com`})
+       
+     }
+   }
+    return {
+      statusCode: response.statusCode,
+       body: JSON.stringify({error: "domain setting failed"})
+       
+     } 
+  } catch(e){
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: e.message})
+    }
+  }
+  
+}
