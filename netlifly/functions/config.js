a5b96ac3f75603de3f253893e9d7f1e4ebd1af9f
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-08-21 20:09:09 +0100

    Update config.js

--- netlifly/functions/config.js
+++ netlifly/functions/config.js
@@ -1,1 +1,47 @@
+const token = process.env.GH_TOKEN;
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: JSON.stringify({ error: "Method Not Allowed" }) };
+  }
+  const schoolId= JSON.parse(event.body).schoolId;
+  const schoolName= JSON.parse(event.body).schoolName;
+  const owner = "ishakuyusufmaina";
+  const url = `https://api.github.com/repos/${owner}/${schoolId}/contents/config.js`;
+  const content = `
+  const schoolId = "${schoolId}"
+  const schoolName = "${schoolName.toUpperCase()}"
+  `;
+  const contentBase64 = Buffer.from(content, "utf-8").toString("base64");
 
+  try {
+    const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/vnd.github+json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
+    const data = await response.json();
+
+   if (response.ok) {
+     return {
+       statusCode: 200,
+       body: JSON.stringify({ok: true, message:  "configuration done"})
+       
+     }
+     
+    return {
+      statusCode: response.statusCode,
+       body: JSON.stringify({error: "configuration failed"})
+       
+     } 
+  } catch(e){
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: e.message})
+    }
+  }
+  
+}
