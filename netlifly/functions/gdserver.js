bfd48b2684aff1cc32c4a1ef2d58c18bd7202840
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-06-07 17:32:44 +0100

    Update gdserver.js

--- netlifly/functions/gdserver.js
+++ netlifly/functions/gdserver.js
@@ -1,12 +1,22 @@
 const jwt = require('jsonwebtoken');
 
+exports.handler = async (event) => {
 
-exports.handler = async () => {
-      const corsHeaders = {
+    const corsHeaders = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
     };
+
+    //  Handle preflight request
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            statusCode: 200,
+            headers: corsHeaders,
+            body: ""
+        };
+    }
+
     try {
 
         const clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
@@ -22,29 +32,23 @@ exports.handler = async () => {
             iat: now
         };
 
-        const assertion = jwt.sign(
-            payload,
-            privateKey,
-            {
-                algorithm: 'RS256',
-                header: {
-                    alg: 'RS256',
-                    typ: 'JWT'
-                }
+        const assertion = jwt.sign(payload, privateKey, {
+            algorithm: 'RS256',
+            header: {
+                alg: 'RS256',
+                typ: 'JWT'
             }
-        );
+        });
 
         const response = await fetch(
             'https://oauth2.googleapis.com/token',
             {
                 method: 'POST',
                 headers: {
-                    'Content-Type':
-                        'application/x-www-form-urlencoded'
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 },
                 body: new URLSearchParams({
-                    grant_type:
-                        'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
                     assertion
                 })
             }
@@ -55,23 +59,14 @@ exports.handler = async () => {
         if (!response.ok) {
             return {
                 statusCode: 500,
-                body: JSON.stringify(data),
-                headers: {
-                     "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-                }
+                headers: corsHeaders,
+                body: JSON.stringify(data)
             };
         }
 
         return {
             statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json'
-                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-            },
+            headers: corsHeaders,
             body: JSON.stringify({
                 access_token: data.access_token,
                 expires_in: data.expires_in
@@ -79,18 +74,12 @@ exports.handler = async () => {
         };
 
     } catch (err) {
-
         return {
             statusCode: 500,
+            headers: corsHeaders,
             body: JSON.stringify({
                 error: err.message
-            }),
-             headers: {
-                     "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-                }
+            })
         };
-
     }
 };
